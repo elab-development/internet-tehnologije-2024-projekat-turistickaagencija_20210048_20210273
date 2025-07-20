0ddd8b5c9247c709717ad3373fecdfae6801c8d5
Danica Djurdjic <danicaadjurdjic@gmail.com>
2025-07-20 20:37:21 +0200

    rute

--- server/routes/api.php
+++ server/routes/api.php
@@ -6,3 +6,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [\App\Http\Controllers\LogovanjeController::class, 'logovanje']);
+Route::post('/register', [\App\Http\Controllers\LogovanjeController::class, 'registracija']);
+
+Route::get('/putovanja', [\App\Http\Controllers\PutovanjeController::class,'index']);
+Route::get('/aranzmani', [\App\Http\Controllers\AranzmaniController::class,'vratiAktivneAranzmane']);
+
+Route::get('/aranzmani/{putovanjeId}', [\App\Http\Controllers\AranzmaniController::class,'vratiAranzmanePoPutovanju']);
+Route::get('/plan-arazmana/{aranzmanId}', [\App\Http\Controllers\PlanAranzmanaController::class,'vratiPlanoveZaAranzman']);
+
+Route::resource('putnici', \App\Http\Controllers\PutnikController::class)->only([
+    'index', 'show'
+]);
+
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/aranzmani', [\App\Http\Controllers\AranzmaniController::class,'noviAranzman']);
+    Route::delete('/aranzmani/{id}', [\App\Http\Controllers\AranzmaniController::class,'obrisiAranzman']);
+    Route::post('/putovanja', [\App\Http\Controllers\PutovanjeController::class,'unesiPutovanje']);
+    Route::delete('/putovanja/{id}', [\App\Http\Controllers\PutovanjeController::class,'obrisiPutovanje']);
+
+    Route::resource('putnici', \App\Http\Controllers\PutnikController::class)->only([
+        'store', 'update', 'destroy'
+    ]);
+
+    Route::get('/grupisano', [\App\Http\Controllers\PutnikController::class,'grupisanoBrojPutnikaPoAranzmanu']);
+    Route::get('/aranzmani/{aranzmanId}/putnici', [\App\Http\Controllers\PutnikController::class,'putniciPoAranzmanu']);
+    // Uplate
+    Route::get('/uplate', [\App\Http\Controllers\UplateController::class, 'uplate']);
+    Route::get('/paginacija', [\App\Http\Controllers\UplateController::class, 'paginacija']);
+    Route::get('/aranzmani/{aranzmanId}/uplate', [\App\Http\Controllers\UplateController::class, 'uplatePoAranzmanu']);
+    Route::post('/uplate', [\App\Http\Controllers\UplateController::class, 'novaUplata']);
+    Route::get('/users/{userId}/uplate', [\App\Http\Controllers\UplateController::class, 'uplateZaKorisnika']);
+});
