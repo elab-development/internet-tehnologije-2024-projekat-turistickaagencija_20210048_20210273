f8e7299dd28f5439fc850d012b4aa4b298962104
Vukasin Karajovic <vukasin2002k@gmail.com>
2025-08-12 19:39:08 +0200

    destinacije

--- klijent/src/stranice/Destinacije.jsx
+++ klijent/src/stranice/Destinacije.jsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import HeaderNaslov from "../komponente/HeaderNaslov";
+import axiosInstance from "../komunikacija/axiosKomunikacija";
+import {Accordion} from "react-bootstrap";
 
 const Destinacije = () => {
+
+    const [destinacije, setDestinacije] = React.useState([]);
+
+    useEffect(() => {
+        axiosInstance.get('/putovanja').then(res => {
+            if (res.data.uspesno === true) {
+                setDestinacije(res.data.podaci);
+            } else {
+                console.error("Greška prilikom dohvaćanja destinacija:", res.statusText);
+            }
+        }).catch(err => {
+            console.error("Došlo je do greške prilikom dohvaćanja destinacija:", err);
+        })
+    }, []);
+
     return (
-        <div>
-            Destinacije stranica
-        </div>
+        <>
+            <HeaderNaslov naslov="Naše destinacije" podnaslov="Istražite naše popularne destinacije" />
+            {
+                destinacije.length > 0 ? (
+                    <>
+                        <Accordion defaultActiveKey="0">
+                            {
+                                destinacije.map((destinacija, index) => (
+                                    <Accordion.Item eventKey={index.toString()} key={destinacija.id}>
+                                        <Accordion.Header className="akordijan-header">{destinacija.nazivPutovanja} ({destinacija.lokacija})</Accordion.Header>
+                                        <Accordion.Body className="akordijan-body">
+                                            {destinacija.opis}
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                ))
+                            }
+                        </Accordion>
+                    </>
+                ) : (
+                    <p>Nema dostupnih destinacija.</p>
+                )
+            }
+        </>
     );
 };
 
